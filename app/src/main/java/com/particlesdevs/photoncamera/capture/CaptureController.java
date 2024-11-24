540b9af0292d9e018eec07ee6cc0b330184cdf21
eszdman <eszdman2015@gmail.com>
2024-11-25 02:25:50 +0600

    Fix capturing empty image

--- app/src/main/java/com/particlesdevs/photoncamera/capture/CaptureController.java
+++ app/src/main/java/com/particlesdevs/photoncamera/capture/CaptureController.java
@@ -1839,7 +1839,8 @@ public class CaptureController implements MediaRecorder.OnInfoListener {
                             });
                             try{
                             mImageSaver.updateFrameCount(mImageSaver.bufferSize());
-                            mImageSaver.runRaw(mCameraCharacteristics, mCaptureResult, mCaptureRequest, new ArrayList<>(BurstShakiness), cameraRotation);
+                            if (mImageSaver.bufferSize() != 0)
+                                mImageSaver.runRaw(mCameraCharacteristics, mCaptureResult, mCaptureRequest, new ArrayList<>(BurstShakiness), cameraRotation);
                             } catch (Exception e){
                                 Log.e(TAG, "runRaw:"+Log.getStackTraceString(e));
                                 cameraEventsListener.onProcessingError(e.getLocalizedMessage());
