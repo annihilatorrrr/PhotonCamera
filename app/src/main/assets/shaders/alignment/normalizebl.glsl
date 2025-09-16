aac4a0aabf39e9aecfa3b8b9258ef8fb23059fe4
eszdman <eszdman2015@gmail.com>
2025-09-16 15:08:23 +0600

    Move deconvolution to bl step

--- app/src/main/assets/shaders/alignment/normalizebl.glsl
+++ app/src/main/assets/shaders/alignment/normalizebl.glsl
@@ -13,9 +13,20 @@ uniform float whiteLevel;
 #define M_PI 3.1415926535897932384626433832795
 #define TILE_AL 16
 
+vec4 getPixelLaplacian(ivec2 coords, highp sampler2D tex) {
+    ivec2 size = textureSize(tex, 0);
+    coords = clamp(coords, ivec2(1), size - ivec2(2));
+    vec4 center = texelFetch(tex, coords, 0);
+    vec4 left = texelFetch(tex, coords + ivec2(-1, 0), 0);
+    vec4 right = texelFetch(tex, coords + ivec2(1, 0), 0);
+    vec4 up = texelFetch(tex, coords + ivec2(0, -1), 0);
+    vec4 down = texelFetch(tex, coords + ivec2(0, 1), 0);
+    return (left + right + up + down) - center * 3.0;
+}
+
 void main() {
     ivec2 xy = ivec2(gl_GlobalInvocationID.xy);
-    vec4 bayer = texelFetch(baseTexture, xy, 0);
+    vec4 bayer = getPixelLaplacian(xy, baseTexture);
     float gains = dot(texture(gainMap, vec2(xy)/vec2(imageSize(outTexture).xy)),vec4(0.25));
     bayer = clamp((bayer - blackLevel) / (vec4(whiteLevel) - blackLevel), 0.0, 1.0);
     imageStore(outTexture, xy, bayer);
