05f72800e6e1d7fdc014ad4fe668c895e773b08f
eszdman <eszdman2015@gmail.com>
2026-07-27 16:01:31 +0600

    Vendor tag refactor

--- app/src/main/java/com/particlesdevs/photoncamera/api/VendorTagUtils.java
+++ app/src/main/java/com/particlesdevs/photoncamera/api/VendorTagUtils.java
@@ -2,7 +2,7 @@ package com.particlesdevs.photoncamera.api;
 
 import android.annotation.SuppressLint;
 import android.hardware.camera2.CaptureRequest;
-import android.os.Build;
+
 import com.particlesdevs.photoncamera.util.Log;
 
 public class VendorTagUtils {
@@ -21,32 +21,50 @@ public class VendorTagUtils {
         }
         return supported;
     }
+
+    @SuppressLint("NewApi")
+    private static <T> void setPhysical(CaptureRequest.Builder builder, CaptureRequest.Key<T> key, T value, String physicalId) {
+        try {
+            builder.setPhysicalCameraKey(key, value, physicalId);
+        } catch (Exception e) {
+            Log.w(TAG, "Error setting physical camera key: " + key.getName() + " for camera: " + physicalId, e);
+        }
+    }
     @SuppressLint({"NewApi", "LocalSuppress"})
-    public static void builderSessionApply(CaptureRequest.Builder builder, boolean burst, boolean useMaximumResolutionKey) {
+    public static void builderSessionApply(CaptureRequest.Builder builder, boolean burst, boolean useMaximumResolutionKey, String physicalId) {
         try {
             byte enable = 1;
              var clientName = new CaptureRequest.Key<>("com.xiaomi.sessionparams.clientName", String.class);
             if(isSupported(builder,clientName)) {
                 Log.d(TAG, "com.xiaomi.sessionparams.clientName supported");
                 builder.set(clientName, "com.android.camera");
+                setPhysical(builder, clientName, "com.android.camera", physicalId);
             }
             if(burst) {
                 var remosaicEnabled = new CaptureRequest.Key<>("xiaomi.remosaic.enabled", Byte.class);
                 if (isSupported(builder, remosaicEnabled)) {
-                    Log.d(TAG, "remosaic.enabled is supported");
                     builder.set(remosaicEnabled, enable);
+                    setPhysical(builder, remosaicEnabled, enable, physicalId);
                 }
                 var remosaicQuadEnabled = new CaptureRequest.Key<>("xiaomi.quadcfa.enabled", Byte.class);
                 if (isSupported(builder, remosaicQuadEnabled)) {
-                    Log.d(TAG, "quadcfa.enabled is supported");
                     builder.set(remosaicQuadEnabled, enable);
+                    setPhysical(builder, remosaicQuadEnabled, enable, physicalId);
                 }
                 var remosaicEnabled2 = new CaptureRequest.Key<>("com.mediatek.control.capture.remosaicenable", int[].class);
                 if (isSupported(builder, remosaicEnabled2)) {
-                    Log.d(TAG, "capture.remosaicenable is supported");
                     builder.set(remosaicEnabled2, new int[]{1});
+                    setPhysical(builder, remosaicEnabled2, new int[]{1}, physicalId);
                 }
             }
+            /*var mode = new CaptureRequest.Key<>("org.codeaurora.qcamera3.sensor_meta_data.current_mode", Integer.class);
+            if (isSupported(builder, mode)) {
+                builder.set(mode, 0);
+            }
+            var mode_index = new CaptureRequest.Key<>("com.qti.sensorbps.mode_index", Integer.class);
+            if (isSupported(builder, mode_index)) {
+                builder.set(mode_index, 0);
+            }*/
         } catch (Exception e){
             Log.w(TAG, "Error applying vendor tags to CaptureRequest.Builder", e);
         }
